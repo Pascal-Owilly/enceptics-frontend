1f5515b442dfdad9645aa1d59b563efc130158a2
Pascal Owilly <owillypascal@gmail.com>
2023-10-23 11:32:15 +0300

    modify login

--- src/components/pages/authenticate/Login.js
+++ src/components/pages/authenticate/Login.js
@@ -97,7 +97,7 @@ const LoginTest = () => {
           type="submit"
           className="btn btn-outline-secondary text-center mt-2 what-card-price btn-sm mt-4"
           style={{ backgroundColor: '#121661', borderColor: '#000092', width:'100%', margin:'auto'}}
-          onClick={handleLoginSubmit}
+          onClick={login}
         >
           Login
         </button>
