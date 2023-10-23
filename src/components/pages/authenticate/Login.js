993480b61d420039f74176b78cdaec3e5b2c555a
Pascal Owilly <owillypascal@gmail.com>
2023-10-23 11:50:46 +0300

    modify login

--- src/components/pages/authenticate/Login.js
+++ src/components/pages/authenticate/Login.js
@@ -67,7 +67,7 @@ const LoginTest = () => {
     <div className='container-fluid' style={{backgroundColor:'#121661'}}>
       <div className='row'>
         <div className='col-md-3'></div>
-        <div className='col-md-6' style={{ height: '100vh', backgroundColor: '#121661', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '17vh' }}>
+        <div className='col-md-6' style={{ height: '100vh', backgroundColor: '#121661', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: '14vh' }}>
         <div >
       <form className='what-card-nav' onSubmit={handleLoginSubmit} style={{ width: '350px', height: '400px', marginTop: '', marginLeft: '', backgroundColor: '#121661' }}>
         <h3 className='text-center text-white'>Login</h3>
